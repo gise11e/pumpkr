53704511846b5243412916b55ee0cf9d32cff319
gi <gise11e@me.com>
2017-01-08 19:16:42 +0000

    server

--- public/js/dist/client.js
+++ public/js/dist/client.js
@@ -4,7 +4,7 @@ function getAndRenderPumpkins() {
   if (event) event.preventDefault();
   return $.ajax({
     method: "GET",
-    url: "http://localhost:8000/pumpkins"
+    url: "https://pumpkr.herokuapp.com/pumpkins"
   }).done(onSuccess).fail(onError);
 }
 
@@ -16,7 +16,7 @@ function postPumpkin() {
 
   return $.ajax({
     method: "POST",
-    url: "http://localhost:8000/pumpkins",
+    url: "https://pumpkr.herokuapp.com/pumpkins",
     data: pumpkin
   }).done(function () {
     window.location = '/';
@@ -26,7 +26,7 @@ function postPumpkin() {
 function deletePumpkin(id) {
   return $.ajax({
     method: "DELETE",
-    url: "http://localhost:8000/pumpkins/" + id
+    url: "https://pumpkr.herokuapp.com/" + id
   }).done(function () {
     return function () {
       $('#' + id).slideUp();
